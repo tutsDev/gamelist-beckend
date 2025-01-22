802548003165a3e06a1daa0f4f5dde4e98498b8e
Arthur Soares Pereira <arthurdev2707@gmail.com>
2025-01-21 23:47:28 -0300

    Projection and GET Games by list

---
+++ GameListApp/src/main/java/com/intensivojavaspring/GameListApp/projections/GameMinProjection.java
@@ -0,0 +1,12 @@
+package com.intensivojavaspring.GameListApp.projections;
+
+public interface GameMinProjection {
+
+    Long getId();
+    String getTitle();
+    Integer getYear();
+    String getImgUrl();
+    String getShortDescription();
+    Integer getPosition();
+
+}
--- GameListApp/src/main/java/com/intensivojavaspring/GameListApp/services/GameService.java
+++ GameListApp/src/main/java/com/intensivojavaspring/GameListApp/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.intensivojavaspring.GameListApp.dto.GameDTO;
 import com.intensivojavaspring.GameListApp.dto.GameMinDTO;
 import com.intensivojavaspring.GameListApp.entities.Game;
+import com.intensivojavaspring.GameListApp.projections.GameMinProjection;
 import com.intensivojavaspring.GameListApp.repositories.GameRepository;
 
 @Service
@@ -28,6 +29,16 @@ public class GameService {
     public List<GameMinDTO> findAll() {
 
         List<Game> result = gameRepository.findAll();
+        
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
